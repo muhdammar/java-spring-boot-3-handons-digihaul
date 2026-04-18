dbe4163f277f59079707beee1c7d111ff6f98912
Ammar <ammar.aziz.work@gmail.com>
2026-04-18 08:33:20 +0800

    first commit

---
+++ src/main/java/com/ammar/javaspringboot3handonsdigihaul/JavaSpringBoot3HandonsDigihaulApplication.java
@@ -0,0 +1,13 @@
+package com.ammar.javaspringboot3handonsdigihaul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBoot3HandonsDigihaulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBoot3HandonsDigihaulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ammar/javaspringboot3handonsdigihaul/JavaSpringBoot3HandonsDigihaulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ammar.javaspringboot3handonsdigihaul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBoot3HandonsDigihaulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
