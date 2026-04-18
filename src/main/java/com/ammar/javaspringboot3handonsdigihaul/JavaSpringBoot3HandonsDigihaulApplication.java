044d5475d36967feacb37e6268c19e8e0161249d
Ammar Abdul Aziz <ammar.aziz.work@gmail.com>
2026-04-18 08:47:19 +0800

    init

--- src/main/java/com/ammar/javaspringboot3handonsdigihaul/JavaSpringBoot3HandonsDigihaulApplication.java
+++ src/main/java/com/ammar/javaspringboot3handonsdigihaul/JavaSpringBoot3HandonsDigihaulApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaSpringBoot3HandonsDigihaulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringBoot3HandonsDigihaulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBoot3HandonsDigihaulApplication.class, args);
+    }
 
 }
--- src/test/java/com/ammar/javaspringboot3handonsdigihaul/JavaSpringBoot3HandonsDigihaulApplicationTests.java
+++ src/test/java/com/ammar/javaspringboot3handonsdigihaul/JavaSpringBoot3HandonsDigihaulApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaSpringBoot3HandonsDigihaulApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
